5b992149fa7edd520909dd79bf34c5d2d32461b1
Maaz4324 <90350901+Maaz4324@users.noreply.github.com>
2023-01-21 23:22:23 +0530

    second commit

--- discord-bot/register-commands.js
+++ discord-bot/register-commands.js
@@ -8,17 +8,15 @@ const commands = [
   },
 ];
 
-const rest = new REST({ version: "10" }).setToken(
-  "MTA2NjM0MDQxMTE2MzAzNzcwNg.GqsgL2.CF9tkULOlFSgMZwFfep1o9ZjkZlPDB0_YkbGSg"
-);
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_KEY);
 (async () => {
   try {
     console.log("Registering slash commands...");
 
     await rest.put(
       Routes.applicationGuildCommands(
-        "1066340411163037706",
-        "1066338460648099921"
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
       ),
       { body: commands }
     );
